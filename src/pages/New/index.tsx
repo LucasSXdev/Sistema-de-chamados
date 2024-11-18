3eb54806053542296d7d89e9c3b0734f9669bca1
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2024-11-17 23:30:45 -0300

    criando e estilizando a pagina de novo chamado

--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,6 +1,7 @@
 import { FiPlusCircle } from "react-icons/fi"
 import Header from "../../components/Header"
 import Title from "../../components/Header/Title"
+import './index.css'
 
 export default function New(){
     return(
@@ -12,8 +13,50 @@ export default function New(){
                 </Title>
 
                 <div className="container">
-                    <form action="
-                    "></form>
+                    <form className="form_profile">
+                        <label>Clientes</label>
+                        <select >
+                            <option key={1} value={1}>Mercado testes</option>
+                            <option key={2} value={2}>Loja informatica</option>
+                        </select>
+
+                        <label>Assunto</label>
+                        <select >
+                            <option key={1} value='suporte'>Suporte</option>
+                            <option key={2} value='visita tecnica'>Visita técnica</option>
+                            <option key={2} value='financeiro'>Financeiro</option>
+                        </select>
+
+                        <label>Status</label>
+                        <div className="status" >
+                            <input type="radio"
+                            name="radio"
+                            value={"aberto"}/>
+
+                            <span>Aberto</span>
+
+                            <input type="radio"
+                            name="radio"
+                            value={"progresso"}/>
+
+                            <span>Em progresso</span>
+
+                            <input type="radio"
+                            name="radio"
+                            value={"atendido"}/>
+
+                            <span>Atendido</span>
+                        </div>
+
+                        <label >Complemento</label>
+                        <textarea 
+                        type = "text"
+                        placeholder="Descreva seu problema (opcional)"/>
+
+                        <button type="submit">Registrar</button>
+
+                        
+                    </form>
                 </div>
             </div>
        </div> 
